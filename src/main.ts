d63dc4d3fc6c2dc6af7b49fce24b7e3779442f3d
mpgxc <mpgx5.c@gmail.com>
2022-04-17 09:39:05 -0300

    feat(v1): adjust entry app module

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 (async () => {
-  const fastify = new FastifyAdapter({
-    logger: true,
-  });
-
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    fastify,
-  );
-
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
 
   await app.listen(
     Number(process.env.APP_PORT) || 3000,
     process.env.APP_HOST || '0.0.0.0',
   );
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  console.log(`Server running 🚀: ${await app.getUrl()}`);
 })();
