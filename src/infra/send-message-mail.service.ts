b1a4ad5c6c6fcc4246f683d027cdce38347c74ca
mpgxc <mpgx5.c@gmail.com>
2022-04-17 21:22:26 -0300

    refactor: decoupled http request responsibility

--- src/infra/send-message-mail.service.ts
+++ src/infra/send-message-mail.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 
-import axios from 'axios';
-
 import { MailQueueProvider } from './providers/queue/mail-queue.provider';
 import { TemplateParseProvider } from './providers/template/template-parse.provider';
 import { MailMessageInput } from './queue/jobs/send-message.interfaces';
@@ -19,13 +17,9 @@ export class SendMessageMailService {
     templateId,
     templateProps,
   }: MailMessageInput): Promise<void> {
-    const { data } = await axios.get(
-      `https://firebasestorage.googleapis.com/v0/b/temvagas-5501c.appspot.com/o/email-sample.html?alt=media&token=aa5b515b-e41a-4686-8b24-9bd916e77179`,
-    );
-
     const template = await this.templateProvider.parse({
-      file: data,
-      props: templateProps,
+      templateId,
+      templateProps,
     });
 
     await this.queueMailProvider.addMany(
