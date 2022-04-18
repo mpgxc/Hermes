cd9fb5b49d3fedab5581bc3bea7d9f9a8f7b83df
mpgxc <mpgx5.c@gmail.com>
2022-04-17 21:07:21 -0300

    fix: resolve validation pipe position

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { AppModule } from './app.module';
 (async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(
     Number(process.env.APP_PORT) || 3000,
     process.env.APP_HOST || '0.0.0.0',
   );
-
-  app.useGlobalPipes(new ValidationPipe());
 
   console.log(`Server running 🚀: ${await app.getUrl()}`);
 })();
